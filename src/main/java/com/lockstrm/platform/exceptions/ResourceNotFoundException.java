8094d2f5bce001c600b61cb29c6333f99a8ae74d
ManuelMNJ <manuelangelnietojimenez@gmail,com>
2026-04-27 23:25:28 +0200

    estable

--- src/main/java/com/lockstrm/platform/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lockstrm.platform.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
